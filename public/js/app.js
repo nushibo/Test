23961d22ea05c3ad774fe231277af001507d1ad2
nushibo <nushiboninja@hotmail.com>
2017-07-06 03:08:39 -0400

    z
    z

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,24 @@
 // app.js
+$('document').ready(function() {
+
+  var webAuth = new auth0.WebAuth({
+    domain: AUTH0_DOMAIN,
+    clientID: AUTH0_CLIENT_ID,
+    redirectUri: AUTH0_CALLBACK_URL,
+    audience: 'https://' + AUTH0_DOMAIN + '/userinfo',
+    responseType: 'token id_token',
+    scope: 'openid'
+  });
+
+  var loginBtn = $('#btn-login');
+
+  loginBtn.click(function(e) {
+    e.preventDefault();
+    webAuth.authorize();
+  });
+
+});
+
 
 $('document').ready(function() {
 
